9dbaa90b9f4c800aac44f25145378c4c62ebafa4
hissue <wldmx@naver.com>
2023-11-09 23:36:11 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/boardhere/boardhere/BoardHereApplication.java
@@ -0,0 +1,13 @@
+package com.boardhere.boardhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardHereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boardhere/boardhere/BoardHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardhere.boardhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
